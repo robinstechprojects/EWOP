82ffb047d240bf4f2e146aaff17f7ebd9113f538
FroznMine <pesafrre@arcor.de>
2014-07-10 21:22:37 +0200

    + lang folder for languages and empty deDe.yml for German. + right now not finished code to load the language + config.yml has new key: language + config.yml some explanation + sorted a bit
    - worlds.yml no longer necessary
    
    Signed-off-by: FroznMine <pesafrre@arcor.de>

--- src/de/froznmine/ewop/Main.java
+++ src/de/froznmine/ewop/Main.java
@@ -1,7 +1,9 @@
 package de.froznmine.ewop;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -13,8 +15,9 @@ import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	private static File welten = new File("plugins/ewop/worlds.yml");
+	private static File configFile = new File("plugins/ewop/config.yml");
 	private static List<String> dontUse, dontBuild, dontBreak;
+	private static HashMap<String, String> language = new HashMap<String, String>();
 
 	public static boolean canUseIn(Player p, String name) {
 		if (dontUse.contains(name) && !p.hasPermission("ewop.use")) return false;
@@ -33,16 +36,25 @@ public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() { 
-		saveResource("worlds.yml", false);
-		FileConfiguration weltenCfg = YamlConfiguration.loadConfiguration(welten);
+		saveConfig();
+		FileConfiguration configCfg = YamlConfiguration.loadConfiguration(configFile);
 		
 		Bukkit.getPluginManager().addPermission(new Permission("ewop.use", "The permission to use things everywhere."));
 		Bukkit.getPluginManager().addPermission(new Permission("ewop.break", "The permission to break things everywhere."));
 		Bukkit.getPluginManager().addPermission(new Permission("ewop.build", "The permission to build things everywhere."));
 		
-		dontUse = weltenCfg.getStringList("dontUse");
-		dontBuild = weltenCfg.getStringList("dontBuild");
-		dontBreak = weltenCfg.getStringList("dontBreak");
+		dontUse = configCfg.getStringList("dontUse");
+		dontBuild = configCfg.getStringList("dontBuild");
+		dontBreak = configCfg.getStringList("dontBreak");
+		
+		//Unused
+		FileConfiguration langCfg = YamlConfiguration.loadConfiguration(new File("plugins/EWOP.jar/" + configCfg.get("language") + ".yml"));
+		
+		Set<String> keys = langCfg.getConfigurationSection("").getKeys(false);
+		for (String key : keys) {
+			language.put(key, langCfg.getString(key));
+		}
+		//Unused end
 		
 		System.out.println("[EWOP] EWOP " + this.getDescription().getVersion() + " loaded without any problems!");
 		Bukkit.getServer().getPluginManager().registerEvents(new EventListener(), this);
